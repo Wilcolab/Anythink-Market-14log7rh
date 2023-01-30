7adaa7c0289a186b7d584725b836aae75ffc861f
kieranjoyce <kieran.joyce99@gmail.com>
2023-01-30 20:11:38 +0000

    filter items by title when search term updated

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -1,8 +1,9 @@
 import React from "react";
+import agent from "../../agent";
 
 export default class SearchBox extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { search: "" };
     this.onChange = this.onChange.bind(this);
   }
@@ -11,11 +12,27 @@ export default class SearchBox extends React.Component {
     this.setState({ search: ev.target.value });
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.search !== this.state.search &&
+      this.state.search.length >= 3
+    ) {
+      const title = this.state.search;
+
+      this.props.onChangeSearch(
+        title,
+        (page) => agent.Items.byTitle(title, page),
+        agent.Items.byTitle(title)
+      );
+    }
+  }
+
   render() {
     return (
-      <form>
+      <form style={{ display: "inline", margin: "0 0.5em" }}>
         <input
           name="search-box"
+          style={{ width: "17em" }}
           ariaLabel="search box"
           id="search-box"
           value={this.state.search}
